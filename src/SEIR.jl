53150002fc5a68727fdeed95c46e1eafd43ade22
Adam Sparks <adamhsparks@icloud.com>
2021-04-10 09:29:17 +0800

    Lint file, remove extra spaces

--- src/SEIR.jl
+++ src/SEIR.jl
@@ -1,24 +1,24 @@
 
 """
     SEIR(wth,
-         emergence,
-         onset,
-         duration,
-         rhlim = 90,
-         rainlim = 5,
-         H0,
-         I0 = 1,
-         RcA,
-         RcT,
-         RcW,
-         RcOpt,
-         p,
-         i,
-         Sx,
-         a,
-         RRS,
-         RRG,
-         RRLEX)
+        emergence,
+        onset,
+        duration,
+        rhlim = 90,
+        rainlim = 5,
+        H0,
+        I0 = 1,
+        RcA,
+        RcT,
+        RcW,
+        RcOpt,
+        p,
+        i,
+        Sx,
+        a,
+        RRS,
+        RRG,
+        RRLEX)
 
 A Susceptible-Exposed-Infectious-Removed (SEIR) model framework originally used
 by specific disease models in EPIRICE to simulate severity of several rice
@@ -27,120 +27,120 @@ diseases.
 Given proper values it can be used with other pathosystems as well.
 
 Returns a DataFrame with the following fields and values.
-   | Field | Value |
-   |-------|-------------|
-   |simday | Zero indexed day of simulation that was run |
-   |dates |  Date of simulation |
-   |sites | Total number of sites present on day "x" |
-   |latent | Number of latent sites present on day "x" |
-   |infectious | Number of infectious sites present on day "x" |
-   |removed | Number of removed sites present on day "x" |
-   |senesced | Number of senesced sites present on day "x" |
-   |rateinf | Rate of infection. \cr
-   |rtransfer | Rate of transfer from latent to infectious sites""
-   |rgrowth | Rate of growth of healthy sites |
-   |rsenesced | Rate of senescence of healthy sites |
-   |rlex | Rate of lesion expansion |
-   |diseased | Number of diseased (latent + infectious + removed) sites |
-   |severity | Disease severity or incidence (for tungro) |
-   |lat | Latitude value as provided by `wth` object |
-   |lon | Longitude value as provided by `wth` object |
+  | Field | Value |
+  |-------|-------------|
+  |simday | Zero indexed day of simulation that was run |
+  |dates |  Date of simulation |
+  |sites | Total number of sites present on day "x" |
+  |latent | Number of latent sites present on day "x" |
+  |infectious | Number of infectious sites present on day "x" |
+  |removed | Number of removed sites present on day "x" |
+  |senesced | Number of senesced sites present on day "x" |
+  |rateinf | Rate of infection. \cr
+  |rtransfer | Rate of transfer from latent to infectious sites""
+  |rgrowth | Rate of growth of healthy sites |
+  |rsenesced | Rate of senescence of healthy sites |
+  |rlex | Rate of lesion expansion |
+  |diseased | Number of diseased (latent + infectious + removed) sites |
+  |severity | Disease severity or incidence (for tungro) |
+  |lat | Latitude value as provided by `wth` object |
+  |lon | Longitude value as provided by `wth` object |
 
 # Arguments
- - wth a data frame of weather on a daily time-step containing data with
+- wth a data frame of weather on a daily time-step containing data with
   the following field names.
-   | Field | value |
-   |-------|-------------|
-   |YYYYMMDD | Date as Year Month Day (ISO8601) |
-   |DOY |  Consecutive day of year, commonly called "Julian date" |
-   |TEMP | Mean daily temperature (°C) |
-   |TMIN | Minimum daily temperature (°C) |
-   |TMAX | Maximum daily temperature (°C) |
-   |TDEW | Mean daily dew point temperature (°C) |
-   |RHUM | Mean daily temperature (°C) |
-   |RAIN | Mean daily rainfall (mm) |
- - emergence expected date of plant emergence entered in `YYYY-MM-DD`
+  | Field | value |
+  |-------|-------------|
+  |YYYYMMDD | Date as Year Month Day (ISO8601) |
+  |DOY |  Consecutive day of year, commonly called "Julian date" |
+  |TEMP | Mean daily temperature (°C) |
+  |TMIN | Minimum daily temperature (°C) |
+  |TMAX | Maximum daily temperature (°C) |
+  |TDEW | Mean daily dew point temperature (°C) |
+  |RHUM | Mean daily temperature (°C) |
+  |RAIN | Mean daily rainfall (mm) |
+- emergence expected date of plant emergence entered in `YYYY-MM-DD`
  format. From Table 1 Savary *et al.* 2012.
- - onset expected number of days until the onset of disease after
+- onset expected number of days until the onset of disease after
  emergence date. From Table 1 Savary *et al.* 2012.
- - duration simulation duration (growing season length). From Table 1
+- duration simulation duration (growing season length). From Table 1
   Savary *et al.* 2012.
- - rhlim threshold to decide whether leaves are wet or not (usually
+- rhlim threshold to decide whether leaves are wet or not (usually
  90 %). From Table 1 Savary *et al.* 2012.
- - rainlim threshold to decide whether leaves are wet or not. From Table
+- rainlim threshold to decide whether leaves are wet or not. From Table
   1 Savary *et al.* 2012.
- - wetness_type simulate RHmax or rain threshold (0) or leaf wetness
+- wetness_type simulate RHmax or rain threshold (0) or leaf wetness
   duration (1). From Table 1 Savary *et al.* 2012.
- - H0 initial number of plant's healthy sites. From Table 1 Savary
+- H0 initial number of plant's healthy sites. From Table 1 Savary
   *et al.* 2012.
- - I0 initial number of infective sites. From Table 1 Savary
+- I0 initial number of infective sites. From Table 1 Savary
   *et al.* 2012.
- - RcA crop age curve for pathogen optimum. From Table 1 Savary
+- RcA crop age curve for pathogen optimum. From Table 1 Savary
   *et al.* 2012.
- - RcT temperature curve for pathogen optimum. From Table 1 Savary
+- RcT temperature curve for pathogen optimum. From Table 1 Savary
   *et al.* 2012.
- - RcW relative curve for pathogen optimum. From Table 1 Savary
+- RcW relative curve for pathogen optimum. From Table 1 Savary
   *et al.* 2012.
- - RcOpt potential basic infection rate corrected for removals. From
+- RcOpt potential basic infection rate corrected for removals. From
   Table 1 Savary *et al.* 2012.
- - i duration of infectious period. From Table 1 Savary
+- i duration of infectious period. From Table 1 Savary
   *et al.* 2012.
  - p duration of latent period. From Table 1 Savary *et al.* 2012.
  - Sx maximum number of sites. From Table 1 Savary *et al.* 2012.
  - a aggregation coefficient. From Table 1 Savary *et al.* 2012.
- - RRS relative rate of physiological senescence. From Table 1 Savary
+- RRS relative rate of physiological senescence. From Table 1 Savary
   *et al.* 2012.
  - RRG relative rate of growth. From Table 1 Savary *et al.* 2012.
- - RRLEX relative rate of lesion expansion. From Table 1 Savary
+- RRLEX relative rate of lesion expansion. From Table 1 Savary
   *et al.* 2015.
  - lesion_size ...  From Table 1 Savary *et al.* 2015.
 
 # References
- > Savary, S., Nelson, A., Willocquet, L., Pangga, I., and Aunario,  J. Modeling and mapping potential epidemics of rice diseases globally. Crop
- Protection, Volume 34, 2012, Pages 6-17, ISSN 0261-2194 DOI: <http://dx.doi.org/10.1016/j.cropro.2011.11.009>.
+> Savary, S., Nelson, A., Willocquet, L., Pangga, I., and Aunario,  J. Modeling and mapping potential epidemics of rice diseases globally. Crop
+Protection, Volume 34, 2012, Pages 6-17, ISSN 0261-2194 DOI: <http://dx.doi.org/10.1016/j.cropro.2011.11.009>.
 
- > Savary, S., Stetkiewicz, S., Brun, F., and Willocquet, L. Modelling and  Mapping Potential Epidemics of Wheat Diseases—Examples on Leaf Rust and
- Septoria Tritici Blotch Using EPIWHEAT. European Journal of Plant Pathology  142, no. 4 (August 1, 2015): 771–90. DOI:
- <https://doi.org/10.1007/s10658-015-0650-7>.
+> Savary, S., Stetkiewicz, S., Brun, F., and Willocquet, L. Modelling and  Mapping Potential Epidemics of Wheat Diseases—Examples on Leaf Rust and
+Septoria Tritici Blotch Using EPIWHEAT. European Journal of Plant Pathology  142, no. 4 (August 1, 2015): 771–90. DOI:
+<https://doi.org/10.1007/s10658-015-0650-7>.
 
 # Examples
 ```jldoctest
- # provide suitable values for brown spot severity
- julia> RcA = [[collect(0:6) * 20],
+# provide suitable values for brown spot severity
+julia> RcA = [[collect(0:6) * 20],
   [0.35, 0.35, 0.35, 0.47, 0.59, 0.71, 1.0]]
- julia> RcT = [[15 .+ (collect(0:5) * 5)],
+julia> RcT = [[15 .+ (collect(0:5) * 5)],
   [0, 0.06, 1.0, 0.85, 0.16, 0]]
- julia> RcW = [[collect(0:8) * 3],
+julia> RcW = [[collect(0:8) * 3],
               [0, 0.12, 0.20, 0.38, 0.46, 0.60, 0.73, 0.87, 1.0]]
- julia> emergence = "2000-07-15"
+julia> emergence = "2000-07-15"
 
- julia> using RCall
+julia> using RCall
 
- julia> wth = rcopy(R"nasapower::get_power(community = 'AG',
+julia> wth = rcopy(R"nasapower::get_power(community = 'AG',
   lonlat = c(151.81, -27.48),
   pars = c('RH2M', 'T2M', 'PRECTOT'),
   dates = c('2000-07-01', '2000-12-31'),
   temporal_average = 'DAILY')")
 
 
- julia> SEIR(
-   wth = wth,
-   emergence = emergence,
-   onset = 20,
-   duration = 120,
-   RcA = RcA,
-   RcT = RcT,
-   RcW = RcW,
-   RcOpt = 0.61,
-   p =  6,
-   i = 19,
-   H0 = 600,
-   a = 1,
-   Sx = 100000,
-   RRS = 0.01,
-   RRG = 0.1
- )
- ```
+julia> SEIR(
+  wth = wth,
+  emergence = emergence,
+  onset = 20,
+  duration = 120,
+  RcA = RcA,
+  RcT = RcT,
+  RcW = RcW,
+  RcOpt = 0.61,
+  p =  6,
+  i = 19,
+  H0 = 600,
+  a = 1,
+  Sx = 100000,
+  RRS = 0.01,
+  RRG = 0.1
+)
+```
 """
 function seir(wth,
               emergence,
@@ -173,7 +173,7 @@ function seir(wth,
 
     # check that the dates roughly align
     if !(emergence_day >= wth[1, "YYYYMMDD"]) ||
-         final_day > findmax(wth[:, "YYYYMMDD"])[1]
+        final_day > findmax(wth[:, "YYYYMMDD"])[1]
       error("incomplete weather data or dates do not align")
     end
 
@@ -223,7 +223,7 @@ function seir(wth,
 
       if wth[!, "RHUM"[cs_2] == rhlim] | wth[!, "RAIN"[cs_2] >= rainlim]
         RHCoef[cs_2] = 1
-       end
+      end
         
       cs_6 = d + 1
       cs_3 = cs_6
