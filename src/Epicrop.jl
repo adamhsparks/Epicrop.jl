9857ddc3e676fb6ff84bbcfb4bf93da41a579d47
Adam Sparks <adamhsparks@gmail.com>
2021-09-29 11:14:54 +0800

    Use README.md as doc

--- src/Epicrop.jl
+++ src/Epicrop.jl
@@ -1,5 +1,11 @@
 module Epicrop
 
+# Use the README as the module docs
+@doc let
+    path = joinpath(dirname(@__DIR__), "README.md")
+    include_dependency(path)
+    read(path, String)
+
 using DataFrames
 using Dates
 using Interpolations
