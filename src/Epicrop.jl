52ef272e9c7d1b3eccf0453ec0380718f90cf40c
Adam Sparks <adamhsparks@icloud.com>
2022-01-03 16:55:56 +0800

    Fix bug preventing package from compiling and rename seir>hlip

--- src/Epicrop.jl
+++ src/Epicrop.jl
@@ -1,18 +1,10 @@
 module Epicrop
 
-# Use the README as the module docs
-@doc let
-    path = joinpath(dirname(@__DIR__), "README.md")
-    include_dependency(path)
-    read(path, String)
+using DataFrames, Dates, Interpolations
 
-using DataFrames
-using Dates
-using Interpolations
+include("hlip.jl")
 
-include("seir.jl")
-
-export run_seir_model
+export run_hlip_model
 
 #re-exports from DataFrames.jl
 export DataFrame
