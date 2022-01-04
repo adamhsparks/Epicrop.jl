3d7d325afb605c7f84e6ef1bbad834d7d98a65fa
Adam Sparks <adamhsparks@gmail.com>
2022-01-04 14:24:53 +0800

    Remove duplicate deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -11,10 +11,3 @@ deploydocs(
     repo = "https://github.com/adamhsparks/Epicrop.jl",
     devbranch = "main"
 )
-
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-deploydocs(
-    repo = "https://github.com/adamhsparks/Epicrop.jl"
-)
