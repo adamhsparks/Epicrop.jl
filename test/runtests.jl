d6ba331939ec2eb4248b0c18d087eadf9c42ef1a
Adam Sparks <adamhsparks@gmail.com>
2022-01-09 16:45:28 +0800

    Let's try using the new downloader that I created

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,11 @@ using Downloads
     downloader = Downloads.Downloader()
     downloader.easy_hook = (easy, info) -> Downloads.Curl.setopt(easy, Downloads.Curl.CURLOPT_LOW_SPEED_TIME, 60)
 
-    w = CSV.read(Downloads.download("https://power.larc.nasa.gov/api/temporal/daily/point?parameters=PRECTOTCORR,T2M,RH2M&community=ag&start=20100701&end=20101028&latitude=14.6774&longitude=121.25562&format=csv&time_standard=utc&user=Epicropjl"), DataFrame, header = 12)
+    w = CSV.read(
+        Downloads.download(
+            "https://power.larc.nasa.gov/api/temporal/daily/point?parameters=PRECTOTCORR,T2M,RH2M&community=ag&start=20100701&end=20101028&latitude=14.6774&longitude=121.25562&format=csv&time_standard=utc&user=Epicropjl",
+            downloader=downloader),
+            DataFrame, header = 12)
 
     # rename the columns to match the expected column names for hlipmodel
     rename!(w, :RH2M => :RHUM, :T2M => :TEMP, :PRECTOTCORR => :RAIN)
