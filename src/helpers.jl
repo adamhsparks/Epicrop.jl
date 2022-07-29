5009fb1d07a5bbc4eca9a1c371801464195f3938
Adam Sparks <adamhsparks@gmail.com>
2022-07-29 16:23:25 +0800

    Reformat helpers.jl

--- src/helpers.jl
+++ src/helpers.jl
@@ -144,30 +144,30 @@ function leafblast(;
     emergence
 )
 
-RcA = [0 1; 5 1; 10 1; 15 0.9; 20 0.8; 25 0.7; 30 0.64; 35 0.59; 40 0.53; 45 0.43; 50 0.32; 55 0.22; 60 0.16; 65 0.09; 70 0.03; 75 0.02]
-RcT = [10 0; 15 0.5; 20 1; 25 0.6; 30 0.2; 35 0.05; 40 0.01; 45 0]
+    RcA = [0 1; 5 1; 10 1; 15 0.9; 20 0.8; 25 0.7; 30 0.64; 35 0.59; 40 0.53; 45 0.43; 50 0.32; 55 0.22; 60 0.16; 65 0.09; 70 0.03; 75 0.02]
+    RcT = [10 0; 15 0.5; 20 1; 25 0.6; 30 0.2; 35 0.05; 40 0.01; 45 0]
 
-return(
-    hlipmodel(
-        wth = wth,
-        emergence = emergence,
-        onset = 20,
-        duration = 120,
-        rhlim = 90,
-        rainlim = 5,
-        H0 = 600,
-        I0 = 1,
-        RcA = RcA,
-        RcT = RcT,
-        RcOpt = 1.14,
-        p = 5,
-        i = 20,
-        Sx = 30000,
-        a = 1.0,
-        RRS = 0.01,
-        RRG = 0.1
+    return (
+        hlipmodel(
+        wth=wth,
+        emergence=emergence,
+        onset=20,
+        duration=120,
+        rhlim=90,
+        rainlim=5,
+        H0=600,
+        I0=1,
+        RcA=RcA,
+        RcT=RcT,
+        RcOpt=1.14,
+        p=5,
+        i=20,
+        Sx=30000,
+        a=1.0,
+        RRS=0.01,
+        RRG=0.1
     )
-)
+    )
 end
 
 
@@ -201,31 +201,31 @@ function sheathblight(;
     emergence
 )
 
-RcA = [0 0.84; 10 0.84; 20 0.84; 30 0.84; 40 0.84; 50 0.84; 60 0.84; 70 0.88; 80 0.88; 90 1; 100 1; 110 1; 120 1]
+    RcA = [0 0.84; 10 0.84; 20 0.84; 30 0.84; 40 0.84; 50 0.84; 60 0.84; 70 0.88; 80 0.88; 90 1; 100 1; 110 1; 120 1]
 
-RcT = [12 0; 16 0.42; 20 94; 24 0.94; 28 1; 32 0.85; 36 0.64; 40 0]
+    RcT = [12 0; 16 0.42; 20 94; 24 0.94; 28 1; 32 0.85; 36 0.64; 40 0]
 
-return(
-    hlipmodel(
-        wth = wth,
-        emergence = emergence,
-        onset = 30,
-        duration = 120,
-        rhlim = 90,
-        rainlim = 5,
-        H0 = 25,
-        I0 = 1,
-        RcA = RcA,
-        RcT = RcT,
-        RcOpt = 0.46,
-        p = 3,
-        i = 120,
-        Sx = 800,
-        a = 2.8,
-        RRS = 0.005,
-        RRG = 0.2
+    return (
+        hlipmodel(
+        wth=wth,
+        emergence=emergence,
+        onset=30,
+        duration=120,
+        rhlim=90,
+        rainlim=5,
+        H0=25,
+        I0=1,
+        RcA=RcA,
+        RcT=RcT,
+        RcOpt=0.46,
+        p=3,
+        i=120,
+        Sx=800,
+        a=2.8,
+        RRS=0.005,
+        RRG=0.2
     )
-)
+    )
 end
 
 """
@@ -258,29 +258,29 @@ function tungro(;
     emergence
 )
 
-RcA = [0 1; 15 1; 30 0.98; 45 0.73; 60 0.51; 75 0.34; 90 0; 105 0; 120 0]
+    RcA = [0 1; 15 1; 30 0.98; 45 0.73; 60 0.51; 75 0.34; 90 0; 105 0; 120 0]
 
-RcT = [9 0; 10 13; 13.1111 0.65; 16.2222 0.75; 19.3333 0.83; 22.4444 0.89; 25.5555 0.93; 28.6666 0.97; 31.7777 1; 34.8889 0.93; 37.9999 0.99; 40 0]
+    RcT = [9 0; 10 13; 13.1111 0.65; 16.2222 0.75; 19.3333 0.83; 22.4444 0.89; 25.5555 0.93; 28.6666 0.97; 31.7777 1; 34.8889 0.93; 37.9999 0.99; 40 0]
 
-return(
-    hlipmodel(
-        wth = wth,
-        emergence = emergence,
-        onset = 30,
-        duration = 120,
-        rhlim = 90,
-        rainlim = 5,
-        H0 = 25,
-        I0 = 1,
-        RcA = RcA,
-        RcT = RcT,
-        RcOpt = 0.18,
-        p = 6,
-        i = 120,
-        Sx = 100,
-        a = 1.0,
-        RRS = 0.01,
-        RRG = 0.1
+    return (
+        hlipmodel(
+        wth=wth,
+        emergence=emergence,
+        onset=30,
+        duration=120,
+        rhlim=90,
+        rainlim=5,
+        H0=25,
+        I0=1,
+        RcA=RcA,
+        RcT=RcT,
+        RcOpt=0.18,
+        p=6,
+        i=120,
+        Sx=100,
+        a=1.0,
+        RRS=0.01,
+        RRG=0.1
     )
-)
+    )
 end
