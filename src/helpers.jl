9c75f23b0ff439acf89fbd5ba6f25e58952b5f31
Adam H. Sparks <adamhsparks@gmail.com>
2022-08-07 14:21:09 +0800

    line breaks in example

--- src/helpers.jl
+++ src/helpers.jl
@@ -29,7 +29,11 @@ using Downloads
 using Epicrop
 
 # download weather data from NASA POWER API
-w = CSV.read(Downloads.download("https://power.larc.nasa.gov/api/temporal/daily/point?parameters=PRECTOTCORR,T2M,RH2M&community=ag&start=20100701&end=20101028&latitude=14.6774&longitude=121.25562&format=csv&time_standard=utc&user=Epicropjl"), DataFrame, header = 12)
+w = CSV.read(
+    Downloads.download(
+        "https://power.larc.nasa.gov/api/temporal/daily/point?parameters=PRECTOTCORR,T2M,RH2M&community=ag&start=20100701&end=20101028&latitude=14.6774&longitude=121.25562&format=csv&time_standard=utc&user=Epicropjl"),
+        DataFrame,
+        header = 12)
 
 # rename the columns to match the expected column names for hlipmodel
 rename!(w, :RH2M => :RHUM, :T2M => :TEMP, :PRECTOTCORR => :RAIN)
